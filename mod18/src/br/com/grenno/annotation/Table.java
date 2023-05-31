7fcd2ecd23fa203232f1b2ddc37889a4793f8e45
Gabriel Renno <gabrielmrenno@gmail.com>
2023-05-31 20:19:22 -0300

    .idea update

--- mod18/src/br/com/grenno/annotation/Table.java
+++ mod18/src/br/com/grenno/annotation/Table.java
@@ -4,6 +4,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+// it can be used on Class and its properties
 @Target({ElementType.FIELD, ElementType.TYPE})
 public @interface Table {
     String value();
